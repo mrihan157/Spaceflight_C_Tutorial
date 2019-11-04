f10ca86be7fa554a935c7e8606599e8115953987
jn3g16 <jn3g16@ecs.soton.ac.uk>
2019-11-04 13:04:17 +0000

    Add first example

---
+++ 1_Basic_Program/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main (void) {
+	printf("This is a test program.\n");
+	
+	unsigned int my_uint = 5;		/* This  is an unsigned integer */
+	int my_int = -25;			/* This is a signed integer */
+	
+	printf("Unsigned integer: %i\n", my_uint);
+	printf("Signed integer: %i\n", my_int);
+
+	char my_character = 'A';	/* A char holds 1 byte representing a character in ASCII */
+
+	char characters[3];		/* An array is specified with its type (char) and length (3) */
+	/* Assignment uses the array index, starting at 0 */
+	characters[0] = my_character;		/* You can use already defined chars */
+	characters[1] = 'B';			/* literal chars */
+	characters[2] = 67;			/* Or even the byte value for 'C' in ascii */
+
+	/* Array access is via the same index */
+	printf("my characters: %c, %c, %c\n", characters[0],
+						characters[1],
+						characters[2]);
+}
